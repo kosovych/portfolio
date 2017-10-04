32c3c0a1e67562ebd16077eebe18681151e9b18f
Yrasoslav Kosovych <bassicinbox@gmail.com>
2017-10-04 20:33:43 +0300

    add musucian

---
+++ musician/js/script.js
@@ -0,0 +1,29 @@
+var $btnTop = $("#btn-top");
+var $content = $("#content");
+var $burgMenu = $("#burg-menu");
+var $content = $("#content");
+var $nav = $(".nav");
+
+$(window).on("scroll", function(){
+	if ($(this).scrollTop() >= 100) {
+		$btnTop.fadeIn();
+	} else {
+		$btnTop.fadeOut();
+	}
+});
+
+$btnTop.on("click", function() {
+	$("html, body").animate({scrollTop:0}, 300)
+	// console.log("re");
+});
+
+$burgMenu.on("click", function(ev){
+	ev.stopPropagation();
+	if (ev.currentTarget === this) {
+		$nav.fadeToggle(100);
+	} else return;
+});
+
+$nav.on("click", function(){
+	$nav.fadeToggle(100);
+});
